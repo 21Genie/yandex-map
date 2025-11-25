d54a4432a78e6386a15b8f9c510d40e07eb42c81
Daniel <muravtsev.danil@mail.ru>
2025-11-25 19:44:17 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { YMaps, Map } from '@pbe/react-yandex-maps'
+
+function App() {
+	return (
+		<YMaps>
+			<div>
+				<Map defaultState={{ center: [55.75, 37.57], zoom: 9 }} />
+			</div>
+		</YMaps>
+	)
+}
+
+export default App
